ee830e78ca8c19fe27b8de200d20d72352727663
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2016-12-27 22:14:23 -0500

    reduce example content

--- src/index.js
+++ src/index.js
@@ -2,14 +2,3 @@
 
 require('./index.pug')
 require('./styles.scss') // The page is now styled
-import Please from 'pleasejs'
-const div = document.getElementById('color')
-const button = document.getElementById('button')
-
-const changeColor = () => div.style.backgroundColor = Please.make_color()
-const newDiv = document.createElement('input')
-newDiv.setAttribute('class', 'answer')
-div.append(newDiv)
-const answer = document.querySelector('.answer')
-const changeText = () => div.innerHTML = `<p>hey ${answer.value}</p>`
-button.addEventListener('click', changeText)
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,16 @@
-// webpack.config.prod.js
+// webpack.config.dev.js
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: ['./src/index.js'],
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/index'
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -18,24 +22,33 @@ module.exports = {
       },
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.pug',
-      filetype: 'pug'
+      template: './src/index.pug'
     }),
- new HtmlWebpackPugPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production')
-  })
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ],
   module: {
     loaders: [{
-      test: /\.css$/,
-      loaders: ['style', 'css']
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'postcss', 'sass-loader']
+    },
+    {
+      test: /\.pug$/,
+      loaders: ['pug-loader']
     },
     {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
-    }]
-  }
+    }
+  ]
+  },
+  postcss: function() {
+  return [autoprefixer({
+    browsers: ['>1%','last 3 versions', 'Firefox >= 20', 'ie 8']
+  })]
 }
+};
